ce36a48fc6faa562961b799e12ddf629a29a2230
Javier Moreno Venegas <51798566+jmorenovenegas@users.noreply.github.com>
2019-06-17 12:03:24 +0200

    Add files via upload

--- RCode/process_subnetworks.R
+++ RCode/process_subnetworks.R
@@ -1,8 +1,8 @@
 ## Process the module subnetworks for linkcomm
-get_gml <- function(interactomefile){
-  i = strsplit(interactomefile, split = "_")[[1]][1]
-  m = strsplit(interactomefile, split = "_")[[1]][4]
-  df = read.csv(file.path('Results/expanded_modules_networks', interactomefile))
+get_gml <- function(subnetworkfile){
+  interactome = strsplit(subnetworkfile, split = "_")[[1]][1]
+  module = strsplit(subnetworkfile, split = "_")[[1]][4]
+  df = read.csv(file = file.path('Results/expanded_modules_networks', subnetworkfile))
   df = df[,2:3]
   names(df) = c("node1", "node2")
   network = graph_from_data_frame(df,directed = F)
@@ -10,5 +10,5 @@ get_gml <- function(interactomefile){
   cl <- clusters(network) # get largest connected component
   lcc = induced.subgraph(network, V(network)[which(cl$membership == which.max(cl$csize))])
   lcc = igraph::simplify(lcc)
-  write_graph(lcc, file = paste('Results/linkcomm', paste(i,"_subnetwork_connected.gml",sep = ""), sep = "/"), format = "gml")
+  write_graph(lcc, file = paste('Results/linkcomm', paste(interactome, "_subnetwork_connected.gml",sep = ""), sep = "/"), format = "gml")
 }
